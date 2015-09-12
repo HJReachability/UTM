b03b500389793a8cacbe1ff275c64ef796476626
Mo Chen <mochen72@gmail.com>
2015-09-11 21:11:43 -0700

    added CDC paper

--- code/platoon/@platoon/createLQR.m
+++ code/platoon/@platoon/createLQR.m
@@ -1,9 +1,7 @@
-function K= createLobj(obj)
-
+function K= createLQR(obj)
 %creates an lqr controller for the quadrotor to follow the path
 % obj is the quadrotor object, and obj.A and obj.B describe the dynamics of
 % the linear dynamics of the quadrotor
 
-
-K=lqr(obj.A,obj.B,diag([1 2 1 2]),eye(2)*.01);
+K = lqr(obj.A, obj.B, diag([1 2 1 2]), eye(2)*.01);
 end
