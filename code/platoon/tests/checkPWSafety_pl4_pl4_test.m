9e2dcf7c6c0fb257b9724538f82859dcad1f4310
MAHESH VASHISHTHA <vashishtha.mahesh@gmail.com>
2015-12-04 16:44:21 -0800

    updated Plane4 Simulation

--- code/platoon/tests/checkPWSafety_pl4_pl4_test.m
+++ code/platoon/tests/checkPWSafety_pl4_pl4_test.m
@@ -6,11 +6,27 @@ addpath('../RS_core')
 tfm = TFM;
 tfm.computeRS('pl4_pl4_safe_V');
 
+
+N = 10;
+for i = 1:N
+  checkPWSafety_pl4_pl4_single(tfm)
+end
+
+end
+
+function checkPWSafety_pl4_pl4_single(tfm)
+
+theta = 2*pi*rand;
+dist2 = 50;
+p1 = -rotate2D([dist2 0], theta) + rand(1,2);
+p2 = [-p1(1) -p1(2)];
+v1 = rotate2D([10 0], theta);
+
+
 % Create Plane4 objects and add them to TFM
-pl1 = Plane4([0 0 0 10]);
-pl2 = Plane4([100 0 pi 10]);
-tfm.regVehicle(pl1);
-tfm.regVehicle(pl2);
+tfm.aas = {};
+tfm.regVehicle(Plane4([p1(1) p1(2) atan2(v1(2),v1(1)) norm([v1(1) v1(2)])]));
+tfm.regVehicle(Plane4([p2(1) p2(2) atan2(-v1(2),-v1(1)) norm([v1(1) v1(2)])]));
 
 % Plot initial setup
 figure;
@@ -18,8 +34,8 @@ for j = 1:length(tfm.aas)
   tfm.aas{j}.plotPosition;
   hold on
 end
-xlim([-10 110])
-ylim([-60 60])
+xlim([-dist2 dist2])
+ylim([-dist2 dist2])
 axis square
 
 % Integration parameters
@@ -42,6 +58,10 @@ for i = 1:length(t)
     
     tfm.aas{j}.updateState(u, tfm.dt);
     tfm.aas{j}.plotPosition;
+    
+    % Plot safety reachable set for the other vehicle
+    tfm.aas{j}.plot_safe_V(tfm.aas{~(j-1)+1}, tfm.pl4_pl4_safe_V, ...
+      tfm.safetyTime);    
   end
   drawnow;
 
