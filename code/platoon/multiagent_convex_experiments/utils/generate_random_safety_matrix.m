75fe13ee6460048a755d09d17a7ce7adee02748e
jennifershih <cyshih55@gmail.com>
2015-12-30 15:40:04 +0800

    added a util function that generates safety value matrix for test stub purpose

--- code/platoon/multiagent_convex_experiments/utils/generate_random_safety_matrix.m
+++ code/platoon/multiagent_convex_experiments/utils/generate_random_safety_matrix.m
@@ -1,8 +1,7 @@
-% This file generates random data for saftey value testing
+% This file generates the data for saftey value testing
 
 function [safety_matrix safety_threshold] = generate_random_safety_matrix(N)
     safety_threshold = 1.5;
-
     safety_matrix = nan(N);
 
     for i=1:N
