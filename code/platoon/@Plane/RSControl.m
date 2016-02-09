3fe650400dc98f0d5b1ade0d1df3f72127cc6b3c
Mo Chen <mochen72@dhcp-46-21.EECS.Berkeley.EDU>
2016-02-09 09:55:16 -0800

    Changed Plane.RSControl to take time as an argument.

--- code/platoon/@Plane/RSControl.m
+++ code/platoon/@Plane/RSControl.m
@@ -1,16 +1,9 @@
-function u = RSControl(obj, BRS)
+function u = RSControl(obj, BRS, t)
 % u = RSControl(obj, BRS)
 
-u = [];
-for i = 1:length(BRS.tau)
-  if eval_u(BRS.g, BRS.data(:,:,:,i), obj.x) <= 0
-    P = extractCostates(BRS.g, BRS.data(:,:,:,i));
-    p = calculateCostate(BRS.g, P, obj.x);
-    u = (p(3) >= 0) * (-BRS.uMax) + (p(3) < 0) * BRS.uMax;
-    return
-  end
-end
-
-% Change this!
+ind = find(BRS.tau <= t, 1, 'first');
+P = extractCostates(BRS.g, BRS.data(:,:,:,ind));
+p = calculateCostate(BRS.g, P, obj.x);
+u = (p(3) >= 0) * (-BRS.uMax) + (p(3) < 0) * BRS.uMax;
 
 end
