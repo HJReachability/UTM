b5a5ee09a0d59750abf51b5c9a5091e199d57847
Mo Chen <mochen72@berkeley.edu>
2016-06-01 21:40:53 -0700

    Added 12D quadrotor dynamics

--- code/platoon/@Quadrotor12D/Quadrotor12D.m
+++ code/platoon/@Quadrotor12D/Quadrotor12D.m
@@ -8,6 +8,7 @@ classdef Quadrotor12D < Vehicle
     % scalar, then the Plane has constant speed
     nx = 12;
     nu = 4;
+    g = 9.81;
     
     % Position, velocity, dimensions
     pdim = 1:3;
@@ -15,10 +16,10 @@ classdef Quadrotor12D < Vehicle
   end
   
   methods
-    function obj = 12Dquadrotor(pos, uMin, uMax,aux_states)
-      % obj = 12Dquadrotor(pos, uMin, uMax, aux_states)
+    function obj = Quadrotor12D(pos, vel, uMin, uMax, aux_states, phys_params)
+      % obj = Quadrotor12D(pos, uMin, uMax, aux_states)
       %
-      % Constructor. Creates a 12Dquadrotor object
+      % Constructor. Creates a Quadrotor12D object
       %
       % Dynamics:
       %     \dot x_1  = x_4
@@ -30,7 +31,9 @@ classdef Quadrotor12D < Vehicle
       %     \dot x_7  = x_10 + \sin x_7 \tan(x_8) x_11 + \cos x_7 \tan(x_8) x_12
       %     \dot x_8  = \cos x_7 x_11 - \sin x_7 x_12
       %     \dot x_9  = (\sin x_7/\cos x_8)*x_11 + (\cos x_7/\cos x_8) x_12
-      %     \dot x_10 = ...
+      %     \dot x_10 = x_11 x_12 (I_y - I_z)/I_x + L/I_x u_2
+      %     \dot x_11 = x_10 x_12 (I_z - I_x)/I_y + L/I_y u_3
+      %     \dot x_12 = x_10 x_11 (I_x - I_y)/I_z + 1/I_z u_4
       %
       % Inputs:
       %   x      - state: [xpos; ypos; theta]
@@ -39,19 +42,68 @@ classdef Quadrotor12D < Vehicle
       %   dMax   - disturbance bounds
       %
       % Output:
-      %   obj       - a Plane object
+      %   obj       - a Quadrotor12D object
       
-      if numel(x) ~= 3
-        error('Initial state does not have right dimension!');
+      %% Process position
+      if numel(pos) ~= 3
+        error('Initial position does not have right dimension!');
       end
       
       if ~iscolumn(pos)
         pos = pos';
       end
 
+      %% Process velocity
+      if numel(vel) ~= 3
+        error('Initial velocity does not have right dimension!');
+      end      
       
-      obj.x = x;
+      if ~iscolumn(vel)
+        vel = vel';
+      end
+      
+      %% Process input bounds
+      if numel(uMin) ~= 4 || numel(uMax) ~= 4
+        error('Control bounds do not have the right dimensions!')
+      end
+      
+      if ~iscolumn(uMin)
+        uMin = uMin';
+      end      
+      
+      if ~iscolumn(uMax)
+        uMax = uMax';
+      end
+      
+      obj.uMin = uMin;
+      obj.uMax = uMax;
+      
+      %% Process auxiliary states
+      if nargin < 5
+        aux_states = zeros(6,1);
+      end
+      
+      if numel(aux_states) ~= 6
+        error('Auxiliary states do not have the right dimension!')
+      end
+      
+      if ~iscolumn(aux_states)
+        aux_states = aux_states';
+      end
+      
+      obj.x = [pos; vel; aux_states];
       obj.xhist = obj.x;
+      
+      %% Process physical parameters
+      if nargin < 6
+        phys_params.I = [2.3951e-5; 2.3951e-5; 3.2347e-5];
+        phys_params.m = 35e-3; % 35g
+        phys_params.L = 65e-3; % 65mm
+      end
+      
+      obj.I = phys_params.I;
+      obj.m = phys_params.m;
+      obj.L = phys_params.L;
     end
-    
+  end % end method
 end % end classdef
