0e56854fa1eb895597a539d980033edc310dc155
Mo Chen <mochen72@berkeley.edu>
2016-07-30 14:32:27 -0700

    moved dynamical systems to helperOC

--- code/platoon/@Plane/legacy/RSControl.m
+++
@@ -1,9 +0,0 @@
-function u = RSControl(obj, BRS, t)
-% u = RSControl(obj, BRS)
-
-ind = find(BRS.tau <= t, 1, 'first');
-P = extractCostates(BRS.g, BRS.data(:,:,:,ind));
-p = calculateCostate(BRS.g, P, obj.x);
-u = (p(3) >= 0) * (-BRS.uMax) + (p(3) < 0) * BRS.uMax;
-
-end
--- code/platoon/@Quadrotor/joinPlatoon.m
+++
@@ -1,9 +0,0 @@
-function u = joinPlatoon(obj, platoon, tfm)
-% u = joinPlatoon(obj, platoon)
-% method of Quadrotor class
-%
-% Requests the control signal to join a platoon from the tfm
-
-u = tfm.joinPlatoon(obj, platoon);
-
-end
