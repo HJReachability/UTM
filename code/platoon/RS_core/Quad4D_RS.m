0e497dc97a77dddad4c7d887bbda320b4de2a491
Mo Chen <mochen72@berkeley.edu>
2016-11-29 15:32:21 -0800

    added TTR to Quad4DCAvoid_RS, and added new example Quad4D_RS

--- code/platoon/RS_core/Quad4D_RS.m
+++ code/platoon/RS_core/Quad4D_RS.m
@@ -1,5 +1,9 @@
-function Quad4DCAvoid_RS()
+function Quad4D_RS(gN)
 % Quad4DCAvoid_RS()
+
+if nargin < 1
+  gN = 51;
+end
 
 %% Common parameters
 small = 0.01;
@@ -7,27 +11,25 @@ targetLower = [-0.2; -inf; -0.2; -inf];
 targetUpper = [0.2; inf; 0.2; inf];
 
 % Grid
-gMin = [-2; -3; -2; -3];
-gMax = [2; 3; 2; 3];
+gMin = [-2; -2; -2; -2];
+gMax = [2; 2; 2; 2];
 uMode = 'max';
-dMode = 'min';
-gN = [35; 55; 35; 55];
+gN = gN*ones(4,1);
 
 % Time
-tMax = 1.5;
+tMax = 0.5;
 dt = 0.01;
 tau = 0:dt:tMax;
 
 % Vehicle
-aMax = [1 1];
-bMax = [1 1];
+uMin = -1;
+uMax = 1;
 
 %% Dynamical systems and subsystems
 Xdims = [1 2];
 Ydims = [3 4];
 
 %% Grids and initial conditions
-g = createGrid(gMin, gMax, gN);
 gX = createGrid(gMin(Xdims), gMax(Xdims), gN(Xdims));
 gY = createGrid(gMin(Ydims), gMax(Ydims), gN(Ydims));
 
@@ -40,14 +42,12 @@ dataY0 = shapeRectangleByCorners(gY, targetLower(Ydims), ...
 sD_X.grid = gX;
 sD_Y.grid = gY;
 
-sD_X.dynSys = Quad4DCAvoid([0;0], aMax, bMax, 0, 0, Xdims);
-sD_Y.dynSys = Quad4DCAvoid([0;0], aMax, bMax, 0, 0, Ydims);
+sD_X.dynSys = Quad4D([0;0], uMin, uMax, Xdims);
+sD_Y.dynSys = Quad4D([0;0], uMin, uMax, Ydims);
 
 sD_X.uMode = uMode;
 sD_Y.uMode = uMode;
 
-sD_X.dMode = dMode;
-sD_Y.dMode = dMode;
 extraArgs.visualize = true;
 extraArgs.deleteLastPlot = true;
 
@@ -59,20 +59,12 @@ vfs.gs = {sD_X.grid; sD_Y.grid};
 vfs.datas = {dataX; dataY};
 vfs.tau = tau;
 vfs.dims = {Xdims; Ydims};
-vf = reconSC(vfs, gMin-small, gMax+small, 'full', 'max');
+vf = reconSC(vfs, gMin-small, gMax+small, 'end', 'max');
 
 %% Visualize
-vslice = [3 2];
+vslice = [1 1.5];
 [g2D, data2D] = proj(vf.g, vf.dataMin, [0 1 0 1], vslice);
 
 figure
 visSetIm(g2D, data2D)
-
-%% TTR
-TTR = TD2TTR(vf.g, vf.data, vf.tau);
-
-[g2D, data2D] = proj(vf.g, TTR, [0 1 0 1], vslice);
-
-figure
-visSetIm(g2D, data2D, 'k', 1)
 end
